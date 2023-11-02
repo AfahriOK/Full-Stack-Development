51f0993c6c49c27318f98373e33ba725f3e5b592
Afahri Kerr <AfahriAKerr91@gmail.com>
2023-11-02 02:13:50 -0400

    changed index page to hbs

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,5 @@
 /* GET travel view */
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
     res.render('travel', {title: 'Travlr Getaways'});
 };
 module.exports = {
